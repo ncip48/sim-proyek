0ee52bc0b2d4a6e033484a4c9fd711d03eb554a3
ncip48 <mbahcip00@gmail.com>
2024-10-31 08:36:44 +0700

    feat: stock out

--- app/Filament/Resources/StockResource/Pages/ManageStocks.php
+++ app/Filament/Resources/StockResource/Pages/ManageStocks.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources\StockResource\Pages;
 
 use App\Filament\Resources\StockResource;
 use App\Models\Item;
+use App\Models\Proyek;
 use App\Models\Stock;
 use Filament\Actions;
 use Filament\Forms\Components\Select;
@@ -19,7 +20,8 @@ class ManageStocks extends ManageRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\Action::make('Stock In')->label('Stock In')
+            Actions\Action::make('Stock In')
+                ->label('Stock In')
                 ->form([
                     Select::make('item_id')
                         ->label('Item')
@@ -41,7 +43,36 @@ class ManageStocks extends ManageRecords
                         'date' => date('Y-m-d')
                     ]);
                 }),
-            Actions\CreateAction::make()->createAnother(false)->label('Stock Out')->color('danger'),
+            Actions\Action::make('Stock Out')
+                ->color('danger')
+                ->label('Stock Out')
+                ->form([
+                    Select::make('item_id')
+                        ->label('Item')
+                        ->options(Item::query()->pluck('name', 'id'))
+                        ->rules('required')
+                        ->searchable(),
+                    Select::make('proyek_id')
+                        ->label('Proyek')
+                        ->options(Proyek::query()->pluck('name', 'id'))
+                        ->rules('required')
+                        ->searchable(),
+                    TextInput::make('qty')
+                        ->label('Qty')
+                        ->rules('required')
+                        ->numeric()
+                        ->autocomplete('off')
+                        ->columnSpanFull()
+                ])
+                ->action(function (array $data): void {
+                    Stock::create([
+                        'item_id' => $data['item_id'],
+                        'proyek_id' => $data['proyek_id'],
+                        'qty' => $data['qty'],
+                        'type' => 'out',
+                        'date' => date('Y-m-d')
+                    ]);
+                }),
         ];
     }
 }
